35e63a7476216ec1aefbcd8b67ac6fd339121332
Poshtaklon <152329594+YuliaDruzhynina@users.noreply.github.com>
2024-04-20 20:56:07 +0300

    Update create-table.sql

--- create-table.sql
+++ create-table.sql
@@ -3,8 +3,8 @@ CREATE TABLE students (
   id INTEGER  PRIMARY KEY AUTOINCREMENT NOT NULL,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
-  phone VARCHAR(30),
-  email VARCHAR(30)
+  student_phone VARCHAR(30),
+  student_email VARCHAR(30)
 );
 
 
